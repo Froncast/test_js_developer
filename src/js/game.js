8640340b7ac665768b04794467cccee1a8989dab
Froncast <50759234+Froncast@users.noreply.github.com>
2020-08-13 15:59:08 +0500

    add timer restart game

--- src/js/game.js
+++ src/js/game.js
@@ -6,22 +6,22 @@ const input = document.querySelector('input');
 const message = document.querySelector('.message')
 const count = document.querySelector('.count')
 const attempts = document.querySelector('.arrAttempts')
-console.log(attempts)
+
 const game = () => {
   const randomNum = getRandomNum(1, 100)
   let numberOfattempts = 0;
   const arrAttempts = [];
-
+  
+  console.log(randomNum)
   return function repeatGame() {
     let num = input.value;
-    if(isNumber(num)) {
+    if (isNumber(num)) {
       let userNum = +num;
-      if(userNum < randomNum) {
+      if (userNum < randomNum) {
         numberOfattempts++
         arrAttempts.push(userNum)
         message.textContent = 'Too Low'
-        console.log(arrAttempts)
-      } else if(userNum > randomNum) {
+      } else if (userNum > randomNum) {
         numberOfattempts++
         arrAttempts.push(userNum)
         message.textContent = 'Too hight'
@@ -29,13 +29,30 @@ const game = () => {
         numberOfattempts++
         message.textContent = 'You found the number'
         count.textContent = `Количество затраченных попыток: ${numberOfattempts}`
-        attempts.textContent = `Ваши попытки: ${arrAttempts.join(', ')}`
+        attempts.textContent = (arrAttempts.length > 0) ? `Ваши попытки: ${arrAttempts.join(', ')}` : ''
+
+        let timer = 5
+        let restart = document.createElement('div')
+        const body = document.querySelector('body')
+        body.appendChild(restart)
+        let intervalRestartGame = setInterval(() => {
+          if(--timer >= 0) {
+            restart.textContent = `До перезапуска игры осталось ${timer} секунд`
+          } else {
+            clearInterval(intervalRestartGame)
+            input.value = ``
+            message.textContent = ``
+            count.textContent = ``
+            attempts.textContent = ``
+            body.removeChild(restart)
+            game()()
+          }
+        }, 1000)
       }
     } else {
       message.textContent = `Number is required`
     }
   }
 }
-console.dir(game())
 const startGame = game()
 btn.addEventListener('click', startGame)
